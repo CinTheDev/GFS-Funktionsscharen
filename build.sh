4b6dffa4b76a65ce53860fbc112e2c0277695e7b
Kevin Müller <mkevin2006@gmail.com>
2024-06-08 22:46:04 +0200

    Add script for putting presentation together

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script will render the animations and compile the presentation program
+# and put it together into a directory
+
+if [[ -d out ]]; then
+    rm -rf out
+fi
+
+mkdir out
+mkdir out/vid
+
+# Presenter
+(
+    cd mp4-presenter
+    cargo build --release
+)
+cp mp4-presenter/target/release/mp4-presenter out/mp4-presenter
+
+# Animations
+(
+    cd animation
+    ./render.sh
+)
+cp -r animation/media/videos/basic/1080p60/*.mp4 out/vid/
