375c9f258162210f0f8e7f52cd85edb535705598
Kevin Müller <mkevin2006@gmail.com>
2024-06-09 15:59:18 +0200

    Implement checksum writing again

--- animation/render.py
+++ animation/render.py
@@ -7,14 +7,17 @@ checksum_dir = '/tmp/gfs-funktionsscharen'
 checksum_file = checksum_dir + '/animation-checksums.txt'
 
 for scene in sys.argv[1:]:
-    subprocess.run(['manim', 'render', '-qh', '-a', '--save-sections', scene])
+    subprocess.run(['manim', 'render', '-qh', '-a', '--save_sections', scene])
 
 if os.path.isdir(checksum_dir):
     shutil.rmtree(checksum_dir)
 
 os.mkdir(checksum_dir)
 
+f = open(checksum_file, "w")
+
 for scene in sys.argv[1:]:
-    checksum = ""
-    subprocess.run(['md5sum', scene], stdout=checksum)
-    print(checksum)
+    checksum = subprocess.check_output(['md5sum', scene])
+    f.write(checksum.decode('utf-8'))
+
+f.close()
