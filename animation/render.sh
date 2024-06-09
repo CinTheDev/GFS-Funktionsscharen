b89a77ddac0b7cc2de68933eacfecfe9d3362e8c
Kevin Müller <mkevin2006@gmail.com>
2024-06-09 14:21:55 +0200

    Extend render script to render specified scenes

--- animation/render.sh
+++ animation/render.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-manim render -qh -a --save_sections opening.py
+declare -a SCENES=(
+    "opening.py"
+    "placeholder.py"
+)
+
+# Render all scenes
+for scene in "${SCENES[@]}"
+do
+    manim render -qh -a --save_sections "$scene"
+done
