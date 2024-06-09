45c76602ad11390ef3966b1871c1ec45adb2ca32
Kevin Müller <mkevin2006@gmail.com>
2024-06-09 16:07:46 +0200

    Have render script check for checksum and skip if possible

--- animation/render.py
+++ animation/render.py
@@ -6,18 +6,36 @@ import subprocess
 checksum_dir = '/tmp/gfs-funktionsscharen'
 checksum_file = checksum_dir + '/animation-checksums.txt'
 
-for scene in sys.argv[1:]:
-    subprocess.run(['manim', 'render', '-qh', '-a', '--save_sections', scene])
+checksums = []
 
-if os.path.isdir(checksum_dir):
+if os.path.isfile(checksum_file):
+    f = open(checksum_file, "r")
+
+    for line in f:
+        checksums.append(line)
+    
+    f.close()
     shutil.rmtree(checksum_dir)
+
+scenes = sys.argv[1:]
+
+for i in range(len(scenes)):
+    if i < len(checksums):
+        checksum = subprocess.check_output(['md5sum', scenes[i]]).decode('utf-8')
+
+        # Skip rendering if checksum matches
+        if checksum == checksums[i]:
+            print(scenes[i] + " already rendered. Skipping...")
+            continue
+
+    subprocess.run(['manim', 'render', '-qh', '-a', '--save_sections', scenes[i]])
 
 os.mkdir(checksum_dir)
 
 f = open(checksum_file, "w")
 
-for scene in sys.argv[1:]:
-    checksum = subprocess.check_output(['md5sum', scene])
-    f.write(checksum.decode('utf-8'))
+for scene in scenes:
+    checksum = subprocess.check_output(['md5sum', scene]).decode('utf-8')
+    f.write(checksum)
 
 f.close()
