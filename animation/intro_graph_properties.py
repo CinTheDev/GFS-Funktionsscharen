81f725743def4f33b01db43c455a8ef6e93ae3aa
Kevin Müller <mkevin2006@gmail.com>
2024-06-13 21:40:42 +0200

    Draw empty graph for properties showcase

--- animation/intro_graph_properties.py
+++ animation/intro_graph_properties.py
@@ -8,7 +8,27 @@ class IntroGraphProperties(Scene):
         self.description()
     
     def graph(self):
-        pass
+        self.next_section("Empty_Graph")
+        screen = FullScreenRectangle()
+
+        # Draw coordinate system
+
+        grid = Axes(
+            x_range=(-5, 5, 1),
+            y_range=(-2, 3, 1),
+            x_length=screen.width,
+            y_length=screen.height,
+            axis_config={
+                'include_numbers': True,
+            },
+        )
+
+        x_label = grid.get_x_axis_label("x")
+        y_label = grid.get_y_axis_label("f(x)")
+        grid_labels = VGroup(x_label, y_label)
+
+        self.add(grid, grid_labels)
+        self.wait()
 
     def description(self):
         pass
