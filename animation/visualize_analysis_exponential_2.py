9e8fecba137b652d470c918a307d6304085f0a94
Kevin Müller <mkevin2006@gmail.com>
2024-06-20 15:58:00 +0200

    Visualize points

--- animation/visualize_analysis_exponential_2.py
+++ animation/visualize_analysis_exponential_2.py
@@ -49,6 +49,58 @@ class VisualizeAnalysisExponentialAdvanced(Scene):
         )
         self.wait()
 
+        self.next_section("h_1")
+
+        self.play(
+            self.param_a.tracker.animate.set_value(1),
+            run_time=2,
+            rate_func=rate_functions.smooth,
+        )
+
+        turning_point = always_redraw(
+            lambda: Dot(point=self.grid.input_to_graph_point(self.get_turning_point(), self.function), color=ORANGE)
+        )
+        inflection_point = always_redraw(
+            lambda: Dot(point=self.grid.input_to_graph_point(self.get_inflection_point(), self.function), color=RED)
+        )
+
+        self.play(
+            Create(turning_point),
+            Create(inflection_point),
+            Flash(turning_point),
+            Flash(inflection_point),
+            run_time=0.5,
+        )
+
+        # TODO: Draw markers
+
+        self.wait()
+
+        self.next_section("h_n1")
+
+        self.play(
+            self.param_a.tracker.animate.set_value(-1),
+            run_time=5,
+            rate_func=rate_functions.smooth,
+        )
+        self.wait()
+
     def graph_function(self, x):
         a = self.param_a.tracker.get_value()
         return x * math.exp(a * x)
+    
+    def get_turning_point(self):
+        a = self.param_a.tracker.get_value()
+
+        if abs(a) < 0.01:
+            return 999999
+
+        return -1.0 / a
+    
+    def get_inflection_point(self):
+        a = self.param_a.tracker.get_value()
+
+        if abs(a) < 0.01:
+            return 999999
+
+        return -2.0 / a
