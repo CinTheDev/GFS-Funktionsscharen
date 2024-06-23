d82a6845b043be9a38f9723948f43eb12a1f05a7
Kevin Müller <mkevin2006@gmail.com>
2024-06-23 13:02:26 +0200

    Animate second example

--- animation/bundles.py
+++ animation/bundles.py
@@ -6,6 +6,7 @@ class Bundles(Scene):
     def construct(self):
         self.transition()
         self.first_example()
+        self.second_example()
     
     def transition(self):
         self.next_section("Transition")
@@ -88,3 +89,107 @@ class Bundles(Scene):
 
             last_pos = tex
             all_equations.add(tex)
+        
+        self.next_section("Clear_First_Example")
+
+        self.play(
+            LaggedStart(
+                [Unwrite(eq) for eq in all_equations],
+                lag_ratio=0.1,
+                run_time=1
+            )
+        )
+        self.wait()
+    
+    def second_example(self):
+        equation = MathTex(r"f_a(x) = x^2 - (a + 2)x + (a - 2)a")
+
+        self.play(
+            Write(equation),
+            run_time=0.5,
+        )
+        self.wait()
+
+        self.next_section("Move_Equation")
+
+        equation.generate_target()
+        equation.target.move_to(UP * 2)
+
+        self.play(
+            MoveToTarget(equation),
+            run_time=1,
+        )
+        #self.wait()
+
+        steps_left = [
+            r"f_{-2}(x) = f_{2}(x)",
+            r"x^2 - (-2 + 2)x + (-2 - 2)(-2) = x^2 - (2 + 2)x + (2 - 2)(2)",
+            r"x^2 + 8 = x^2 - 4x",
+            r"8 = -4x",
+            r"x = 2",
+        ]
+        steps_right = [
+            r"f_a(2) = (2)^2 - (a + 2)(2) + (a - 2)a",
+            r"f_a(2) = 4 - 2a + 4 + a^2 - 2a",
+            r"f_a(2) = 8 - 4a + a^2",
+        ]
+
+        last_pos = equation
+        equations_left = VGroup(equation)
+
+        for step in steps_left:
+            tex = MathTex(step)
+            tex.next_to(last_pos, DOWN)
+
+            self.play(
+                FadeIn(tex, shift=UP),
+                run_time=0.5,
+            )
+            self.wait()
+            self.next_section("Solve_left")
+
+            last_pos = tex
+            equations_left.add(tex)
+        
+        self.next_section("Move_equations")
+
+        top = MathTex(r"x = 2")
+        top.move_to(UP * 2)
+
+        self.play(
+            Transform(equations_left, top),
+            run_time=1,
+        )
+
+        last_pos = top
+        equations_right = VGroup(top)
+
+        for step in steps_right:
+            tex = MathTex(step)
+            tex.next_to(last_pos, DOWN)
+
+            self.play(
+                FadeIn(tex, shift=LEFT),
+                run_time=0.5,
+            )
+            self.wait()
+            self.next_section("Solve_right")
+
+            last_pos = tex
+            equations_right.add(tex)
+        
+        self.next_section("Cross")
+
+        comment = Tex("Ist KEIN Funktionsbündel!", color=RED)
+        comment.next_to(equations_right, DOWN)
+        underline = Underline(comment, color=RED)
+
+        self.play(
+            Write(comment),
+            run_time=1,
+        )
+        self.play(
+            Create(underline),
+            run_time=1,
+        )
+        self.wait()
