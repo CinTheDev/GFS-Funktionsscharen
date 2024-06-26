30a1c8d1175c415ac163c42fb23425ca3094d36c
Kevin Müller <mkevin2006@gmail.com>
2024-06-26 21:24:15 +0200

    Animate simple point insertion

--- animation/point_analysis.py
+++ animation/point_analysis.py
@@ -5,6 +5,7 @@ from manim import *
 class PointAnalysis(Scene):
     def construct(self):
         self.graph()
+        self.simple_insertion()
     
     def graph(self):
         self.next_section("Draw_Graph")
@@ -28,7 +29,7 @@ class PointAnalysis(Scene):
 
         x_label = self.grid.get_x_axis_label("x")
         y_label = self.grid.get_y_axis_label("f(x)")
-        grid_labels = VGroup(x_label, y_label)
+        self.grid_labels = VGroup(x_label, y_label)
 
         self.param_a = Variable(
             0,
@@ -36,10 +37,10 @@ class PointAnalysis(Scene):
             num_decimal_places=2,
         )
 
-        function_equation = MathTex(r"f_{{ a }}(x) = 2x^2 - 8{{ a }}x + 9{{ a }}^2")
-        function_equation.set_color_by_tex("a", color=PURPLE)
+        self.function_equation = MathTex(r"f_{{ a }}(x) = 2x^2 - 8{{ a }}x + 9{{ a }}^2")
+        self.function_equation.set_color_by_tex("a", color=PURPLE)
 
-        equations = VGroup(function_equation, self.param_a)
+        equations = VGroup(self.function_equation, self.param_a)
         equations.arrange(DOWN)
         equations.move_to(LEFT * 4 + DOWN * 3)
         equations.add_background_rectangle(opacity=1, stroke_width=1, stroke_opacity=1, stroke_color=ORANGE, buff=0.1)
@@ -53,7 +54,7 @@ class PointAnalysis(Scene):
 
         self.play(
             Create(self.grid),
-            Write(grid_labels),
+            Write(self.grid_labels),
             Write(self.base_function),
             FadeIn(equations),
             run_time=2,
@@ -78,6 +79,64 @@ class PointAnalysis(Scene):
         )
         self.wait()
     
+    def simple_insertion(self):
+        self.next_section("Transition")
+
+        self.play(
+            Unwrite(self.base_function),
+            Unwrite(self.grid_labels),
+            Unwrite(self.param_a),
+            run_time=1,
+        )
+
+        self.simple_insertion_function = always_redraw(
+            lambda: self.grid.plot(
+                lambda a: self.graph_point_function(a),
+                color=PURPLE,
+            )
+        )
+        new_labels = VGroup(
+            self.grid.get_x_axis_label("a"),
+            self.grid.get_y_axis_label("f_a(x)"),
+        )
+        new_param = Variable(
+            0,
+            Tex("x", color=RED),
+            num_decimal_places=2,
+        )
+        new_param.move_to(self.param_a)
+
+        self.grid_labels.become(new_labels)
+        self.param_a.become(new_param)
+
+        self.play(
+            Write(self.simple_insertion_function),
+            Write(self.grid_labels),
+            Write(self.param_a),
+            run_time=2,
+        )
+        self.wait()
+
+        self.next_section("Move_left")
+
+        self.play(
+            self.param_a.tracker.animate.set_value(-3),
+            run_time=1,
+        )
+        self.wait()
+
+        self.next_section("Move_center")
+
+        self.play(
+            self.param_a.tracker.animate.set_value(0),
+            run_time=2,
+        )
+        self.wait()
+    
     def graph_function(self, x):
         a = self.param_a.tracker.get_value()
+        return 2 * x**2 - 8 * a * x + 9 * a**2
+
+    def graph_point_function(self, a):
+        x = self.param_a.tracker.get_value()
         return 2 * x**2 - 8 * a * x + 9 * a**2
