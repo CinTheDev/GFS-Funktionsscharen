36771fdc05c7e6a5b8b6c95ec17690dda6abae00
Kevin Müller <mkevin2006@gmail.com>
2024-06-29 19:01:55 +0200

    Remove old code

--- animation/render.py
+++ animation/render.py
@@ -3,24 +3,6 @@ import os
 import shutil
 import subprocess
 
-"""
-checksum_dir = 'media/checksums'
-checksum_file = checksum_dir + '/animation-checksums.txt'
-checksum_file_old = checksum_dir + '/animation-checksums.old.txt'
-
-checksums = []
-
-if os.path.isfile(checksum_file):
-    f = open(checksum_file, "r")
-
-    for line in f:
-        checksums.append(line)
-    
-    f.close()
-    os.rename(checksum_file, checksum_file_old)
-
-"""
-
 scenes = sys.argv[1:]
 
 for i in range(len(scenes)):
@@ -39,16 +21,6 @@ for i in range(len(scenes)):
             print(scene_filename + " already rendered. Skipping...")
             continue
 
-    """
-    if i < len(checksums):
-        checksum = subprocess.check_output(['md5sum', scene_filename]).decode('utf-8')
-
-        # Skip rendering if checksum matches
-        if checksum == checksums[i]:
-            print(scene_filename + " already rendered. Skipping...")
-            continue
-    """
-
     # Remove sections
     sections_path = 'media/videos/' + scenes[i] + '/1080p60/sections/'
     if os.path.isdir(sections_path):
@@ -63,17 +35,3 @@ for i in range(len(scenes)):
     checksum_io = open(checksum_file, "w")
     checksum_io.write(checksum)
     checksum_io.close()
-
-"""
-if not os.path.isdir(checksum_dir):
-    os.mkdir(checksum_dir)
-
-f = open(checksum_file, "w")
-
-for scene in scenes:
-    scene_filename = scene + '.py'
-    checksum = subprocess.check_output(['md5sum', scene_filename]).decode('utf-8')
-    f.write(checksum)
-
-f.close()
-"""
