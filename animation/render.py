a3617a2cce2002ba086df4a30e8e747cc2994822
Kevin Müller <mkevin2006@gmail.com>
2024-06-29 19:01:26 +0200

    Have checksums stored in target directory

--- animation/render.py
+++ animation/render.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import subprocess
 
+"""
 checksum_dir = 'media/checksums'
 checksum_file = checksum_dir + '/animation-checksums.txt'
 checksum_file_old = checksum_dir + '/animation-checksums.old.txt'
@@ -18,10 +19,27 @@ if os.path.isfile(checksum_file):
     f.close()
     os.rename(checksum_file, checksum_file_old)
 
+"""
+
 scenes = sys.argv[1:]
 
 for i in range(len(scenes)):
     scene_filename = scenes[i] + '.py'
+    scene_dir = "media/videos/" + scenes[i]
+    checksum_file = scene_dir + "/checksum.txt"
+
+    if os.path.isfile(checksum_file):
+        checksum_io = open(checksum_file, "r")
+        saved_checksum = checksum_io.read()
+        checksum_io.close()
+
+        current_checksum = subprocess.check_output(['md5sum', scene_filename]).decode('utf-8')
+
+        if saved_checksum == current_checksum:
+            print(scene_filename + " already rendered. Skipping...")
+            continue
+
+    """
     if i < len(checksums):
         checksum = subprocess.check_output(['md5sum', scene_filename]).decode('utf-8')
 
@@ -29,6 +47,7 @@ for i in range(len(scenes)):
         if checksum == checksums[i]:
             print(scene_filename + " already rendered. Skipping...")
             continue
+    """
 
     # Remove sections
     sections_path = 'media/videos/' + scenes[i] + '/1080p60/sections/'
@@ -38,6 +57,14 @@ for i in range(len(scenes)):
     # Render the scene
     subprocess.run(['manim', 'render', '-qh', '-a', '--save_sections', scene_filename])
 
+    # Save checksum of file
+    checksum = subprocess.check_output(["md5sum", scene_filename]).decode("utf-8")
+
+    checksum_io = open(checksum_file, "w")
+    checksum_io.write(checksum)
+    checksum_io.close()
+
+"""
 if not os.path.isdir(checksum_dir):
     os.mkdir(checksum_dir)
 
@@ -49,3 +76,4 @@ for scene in scenes:
     f.write(checksum)
 
 f.close()
+"""
