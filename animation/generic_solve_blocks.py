3825f44e412b0f7934b981e127d478148e266cf3
Kevin Müller <mkevin2006@gmail.com>
2024-06-29 17:12:52 +0200

    Animate steps

--- animation/generic_solve_blocks.py
+++ animation/generic_solve_blocks.py
@@ -20,7 +20,7 @@ class GenericSolveBlocks(Scene):
             run_time=1,
         )
     
-    def block(self, heading, pos, equations, colored_var=None, highlighted=[], wrong=False, scale=1, invincible=False):
+    def block(self, heading, pos, equations, colored_var=None, highlighted=[], wrong=False, scale=1, invincible=False, math=True, aligned_edge=False):
         top = Tex(heading, color=self.default_color)
         top.scale(0.6)
         top.move_to(pos)
@@ -29,16 +29,22 @@ class GenericSolveBlocks(Scene):
 
         # Construct MathTex equations
         for string in equations:
-            tex = MathTex(
-                string,
-                substrings_to_isolate=colored_var,
-            )
-            if colored_var is not None:
-                tex.set_color_by_tex(colored_var, color=PURPLE)
+            tex = None
+
+            if math:
+                tex = MathTex(
+                    string,
+                    substrings_to_isolate=colored_var,
+                )
+
+                if colored_var is not None:
+                    tex.set_color_by_tex(colored_var, color=PURPLE)
+            else:
+                tex = Tex(string)
             
             tex.scale(scale)
 
-            tex.next_to(top, DOWN)
+            tex.next_to(top, DOWN, aligned_edge=aligned_edge)
 
             all_equations.add(tex)
             top = tex
@@ -84,3 +90,4 @@ class GenericSolveBlocks(Scene):
             self.blocks.append(all_equations)
         
         self.wait()
+        return all_equations
--- animation/practice_point_analysis.py
+++ animation/practice_point_analysis.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
-from generic_solve_block import *
+from generic_solve_blocks import *
 
 class PracticePointAnalysis(GenericSolveBlocks):
     def construct(self):
-        self.transition()
-    
-    def transition(self):
-        pass
+        self.write_steps()
+
+    def write_steps(self):
+        steps = [
+            r"1. Extremstelle herausfinden",
+            r"2. $ x $ in $ f_a(x) $ einsetzen",
+            r"\raggedright 3. Neue Funktion auf \\ \:\:\:\: Extremstellen überprüfen",
+        ]
+
+        self.block("Schritte", LEFT * 6 + UP * 3, steps, scale=0.6, invincible=True, math=False, aligned_edge=LEFT)
+
