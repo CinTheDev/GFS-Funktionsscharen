615b28e12f09b0bcae7867f3b093d9bfe097e2c7
Kevin Müller <mkevin2006@gmail.com>
2024-06-29 17:55:51 +0200

    Animate first problem

--- animation/practice_point_analysis.py
+++ animation/practice_point_analysis.py
@@ -9,6 +9,9 @@ class PracticePointAnalysis(GenericSolveBlocks):
         self.write_steps()
         self.introduce_problems()
 
+        self.introduce_first_problem()
+        self.solve_first_problem()
+
     def write_steps(self):
         steps = [
             r"1. Extremstelle herausfinden",
@@ -26,7 +29,6 @@ class PracticePointAnalysis(GenericSolveBlocks):
             color=ORANGE,
         )
         self.title.scale(0.8)
-        #title.move_to(UP * 3)
 
         self.page_num = Tex("Seite Nummer 164 Nummer 11")
         self.page_num.scale(0.4)
@@ -51,4 +53,49 @@ class PracticePointAnalysis(GenericSolveBlocks):
             MoveToTarget(self.title),
             run_time=1,
         )
+    
+    def introduce_first_problem(self):
+        equation = MathTex(r"f_t(x) = x^2 - tx + 4t^2 - 3t")
+        self.blocks.append(equation)
+
+        self.play(
+            Write(equation),
+            run_time=0.5,
+        )
         self.wait()
+
+        self.next_section("Move_equation")
+
+        equation.generate_target()
+        equation.target.move_to(UP * 1.5)
+        equation.target.scale(0.8)
+
+        self.play(
+            MoveToTarget(equation),
+            run_time=1,
+        )
+        self.wait()
+    
+    def solve_first_problem(self):
+        steps_solve_first = [
+            r"f'_t(x) = 2x - t",
+            r"2x - t = 0",
+            r"2x = t",
+            r"x = 0.5t",
+        ]
+        steps_create_function = [
+            r"g(t) = f_t(0.5t)",
+            r"g(t) = (0.5t)^2 - t(0.5t) + 4t^2 - 3t",
+            r"g(t) = 0.25t^2 - 0.5t^2 + 4t^2 - 3t",
+            r"g(t) = 3.75t^2 - 3t",
+        ]
+        steps_solve_second = [
+            r"g'(t) = 7.5t - 3",
+            r"7.5t - 3 = 0",
+            r"7.5t = 3",
+            r"t = 0.4",
+        ]
+
+        self.block("Extremstellen von f", RIGHT * 5 + UP * 2, steps_solve_first)
+        self.block("Höhenfunktion der Extremstelle", LEFT * 3, steps_create_function)
+        self.block("Extremstellen der Höhenfunktion", RIGHT * 4.25 + DOWN * 1.1, steps_solve_second)
