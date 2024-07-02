b35ab918c75dc8be51a8b3f079f2efe6088f825f
Kevin Müller <mkevin2006@gmail.com>
2024-07-02 15:07:38 +0200

    Animate second problem

--- animation/practice_locus.py
+++ animation/practice_locus.py
@@ -9,6 +9,14 @@ class Practice_Locus(GenericSolveBlocks):
 
         self.introduce_first_problem()
         self.solve_first_problem()
+        self.clear_blocks()
+
+        self.introduce_second_problem()
+        self.solve_second_problem()
+        self.clear_blocks()
+
+        self.introduce_third_problem()
+        self.solve_third_problem()
     
     def transition(self):
         title = Tex("Bestimme die Ortskurve aller Tiefpunkte", color=RED)
@@ -58,3 +66,56 @@ class Practice_Locus(GenericSolveBlocks):
         self.block("Extremstelle", UP * 2 + LEFT * 4, solve_turning_point)
         self.block("Tiefpunkt", UP * 2, solve_height)
         self.block("Umwandeln", UP * 2 + RIGHT * 4, solve_finally)
+    
+    def introduce_second_problem(self):
+        equation = MathTex(r"f_a(x) = e^x - ax")
+        self.blocks.append(equation)
+
+        self.play(
+            Write(equation),
+            run_time=1,
+        )
+        self.wait()
+
+        self.next_section("Make_room")
+
+        equation.generate_target()
+        equation.target.scale(0.8)
+        equation.target.move_to(UP * 3)
+
+        self.play(
+            MoveToTarget(equation),
+            run_time=1
+        )
+    
+    def solve_second_problem(self):
+        solve_turning_point = [
+            r"f'_a(x) = e^x - a",
+            r"e^x - a = 0",
+            r"e^x = a",
+            r"x = ln(a)",
+        ]
+        solve_height = [
+            r"y = f_a(ln(a))",
+            r"y = e^{ln(a)} - ln(a) a",
+            r"y = a - ln(a) a",
+        ]
+        solve_a_x = [
+            r"x = ln(a)",
+            r"a = e^x",
+        ]
+        solve_function = [
+            r"y = e^x - ln(e^x) e^x",
+            r"y = e^x - x e^x",
+        ]
+
+        self.block("Extremstelle", UP * 2 + LEFT * 5, solve_turning_point)
+        self.block("Tiefpunkt", UP * 2, solve_height)
+        self.block("Nach a auflösen", UP * 2 + RIGHT * 5, solve_a_x)
+        self.block("Einsetzen", RIGHT * 5, solve_function)
+    
+    def introduce_third_problem(self):
+        pass
+    
+    def solve_third_problem(self):
+        pass
