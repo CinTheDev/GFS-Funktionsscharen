7864fe62bad095d2c88d767de65d71995d9e333b
Kevin Müller <mkevin2006@gmail.com>
2024-07-02 08:24:24 +0200

    Animate transition

--- animation/locus.py
+++ animation/locus.py
@@ -2,9 +2,17 @@
 
 # Ortskurven
 
+from manim import *
+
 class Locus(Scene):
     def construct(self):
         self.transition()
     
     def transition(self):
-        pass
+        self.next_section("Transition")
+        title = Tex("Ortskurven", color=RED)
+
+        self.play(
+            SpinInFromNothing(title)
+        )
+        self.wait()
