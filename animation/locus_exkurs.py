5b23cd7c45694e09a073cdb3bab42b0b78a9f72c
Kevin Müller <mkevin2006@gmail.com>
2024-07-02 10:19:29 +0200

    Animate the transition to the graph

--- animation/locus_exkurs.py
+++ animation/locus_exkurs.py
@@ -23,3 +23,45 @@ class ExkursParametricFunction(Scene):
             run_time=1,
         )
         self.wait()
+
+        self.next_section("Transition_to_graph")
+
+        screen = FullScreenRectangle()
+
+        self.grid = NumberPlane(
+            x_range=(-5, 5, 1),
+            y_range=(-3, 3, 1),
+            x_length=screen.width,
+            y_length=screen.height,
+            tips=True,
+            background_line_style={
+                "stroke_color": RED,
+                "stroke_opacity": 0.6,
+            },
+            axis_config={
+                "include_numbers": True,
+                "tip_shape": StealthTip,
+            },
+        )
+
+        x_label = self.grid.get_x_axis_label("x")
+        y_label = self.grid.get_y_axis_label("y")
+
+        graph_screen = VGroup(self.grid, x_label, y_label)
+
+        # Remember position before moving away
+        graph_screen.generate_target()
+
+        graph_screen.next_to(screen, DOWN)
+
+        self.add(graph_screen)
+
+        title.generate_target()
+        title.target.shift(UP * 6)
+
+        self.play(
+            MoveToTarget(graph_screen),
+            MoveToTarget(title),
+            run_time=2,
+        )
+        self.wait()
