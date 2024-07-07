b6df8791f9a816e7643fff051e9f60e5f23f8060
Kevin Müller <mkevin2006@gmail.com>
2024-07-07 13:09:41 +0200

    Remove old introductions to problems

--- animation/practice_1.py
+++ animation/practice_1.py
@@ -7,17 +7,14 @@ class Practice_1(GenericSolveBlocks):
         self.transition()
         self.write_all_problems()
 
-        self.write_first_problem()
         self.solve_first_problem()
         self.clear_blocks()
 
-        #self.write_second_problem()
-        #self.solve_second_problem()
-        #self.clear_blocks()
+        self.solve_second_problem()
+        self.clear_blocks()
 
-        #self.write_third_problem()
-        #self.solve_third_problem()
-        #self.clear_blocks()
+        self.solve_third_problem()
+        self.clear_blocks()
 
         #self.write_integral_problem()
         #self.solve_integral_problem()
@@ -97,63 +94,20 @@ class Practice_1(GenericSolveBlocks):
             run_time=0.7,
         )
     
-    def write_first_problem(self):
-        self.equation = MathTex(r"f_a(x) = x^2 - ax")
-
-        comment = Tex("Berechne die Nullpunkte der Funktionenschar.", color=YELLOW)
-        comment.scale(0.6)
-        comment.next_to(self.equation, UP)
-
-        self.play(
-            Write(self.equation),
-            FadeIn(comment, shift=UP),
-            run_time=0.7,
-        )
-        self.wait()
-
-        self.next_section("Make_Room")
-
-        self.equation.generate_target()
-        self.equation.target.move_to(UP * 3)
-
-        self.play(
-            FadeOut(comment),
-            run_time=0.5,
-        )
-        self.play(
-            MoveToTarget(self.equation),
-            run_time=0.5,
-        )
-        self.wait()
-    
     def solve_first_problem(self):
         steps = [
+            r"f_a(x) = x^2 - ax",
             r"f_a(x) = 0",
             r"x^2 - ax = 0",
             r"x(x - a) = 0",
             r"x_1 = 0; x_2 = a"
         ]
 
-        self.block("Lösung", UP * 2, steps)
-    
-    def write_second_problem(self):
-        self.play(
-            Unwrite(self.equation),
-            run_time=0.5,
-        )
-
-        new_equation = MathTex(r"f_a(x) = \frac{x^2 - a^2}{x}")
-        new_equation.move_to(self.equation)
-        self.equation.become(new_equation)
-
-        self.play(
-            Write(self.equation),
-            run_time=0.5,
-        )
-        self.wait()
+        self.block("Erste Aufgabe", UP * 2, steps)
 
     def solve_second_problem(self):
         steps = [
+            r"f_a(x) = \frac{x^2 - a^2}{x}",
             r"\frac{x^2 - a^2}{x} = 0",
             r"x^2 - a^2 = 0",
             r"x^2 = a^2",
@@ -161,33 +115,18 @@ class Practice_1(GenericSolveBlocks):
             r"x_{1;2} \neq 0",
         ]
 
-        self.block("Lösung", UP * 2, steps, highlighted=[-1])
-    
-    def write_third_problem(self):
-        self.play(
-            Unwrite(self.equation),
-            run_time=0.5,
-        )
-
-        new_equation = MathTex(r"f_a(x) = e^{\frac{x}{a}} - a")
-        new_equation.move_to(self.equation)
-        self.equation.become(new_equation)
-
-        self.play(
-            Write(self.equation),
-            run_time=0.5,
-        )
-        self.wait()
+        self.block("Zweite Aufgabe", UP * 2.5, steps, highlighted=[-1])
     
     def solve_third_problem(self):
         steps = [
+            r"f_a(x) = e^{\frac{x}{a}} - a",
             r"e^{\frac{x}{a}} - a = 0",
             r"e^{\frac{x}{a}} = a",
             r"\frac{x}{a} = ln(a)",
             r"x = ln(a) \cdot a",
         ]
 
-        self.block("Lösung", UP * 2, steps)
+        self.block("Dritte Aufgabe", UP * 2, steps)
     
     def write_integral_problem(self):
         self.play(
