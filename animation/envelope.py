c1ad4d7ee102f5c4a75e8be5c9e61ea395d443af
Kevin Müller <mkevin2006@gmail.com>
2024-07-07 15:01:50 +0200

    Create graph

--- animation/envelope.py
+++ animation/envelope.py
@@ -23,4 +23,35 @@ class Envelope(Scene):
         )
     
     def graph(self):
-        pass
+        screen = FullScreenRectangle()
+
+        x_range=(-10, 10, 2)
+        dx = x_range[1] - x_range[0]
+        dy = dx * (screen.height / screen.width)
+        y_range=(-1, -1 + dy, 2)
+
+        grid = NumberPlane(
+            x_range=x_range,
+            y_range=y_range,
+            x_length=screen.width,
+            y_length=screen.height,
+            background_line_style={
+                "stroke_color": PINK,
+                "stroke_opacity": 0.6,
+            },
+            axis_config={
+                "include_numbers": True,
+            },
+        )
+
+        x_label = grid.get_x_axis_label("x")
+        y_label = grid.get_y_axis_label("f(x)")
+        grid_labels = VGroup(x_label, y_label)
+
+        self.graph = VGroup(grid, grid_labels)
+
+        self.play(
+            Create(self.graph),
+            run_time=2,
+        )
+        self.wait()
