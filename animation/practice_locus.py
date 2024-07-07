af6a7c0bbb3cedf94c7b9f0e25cd23e52afc850e
Kevin Müller <mkevin2006@gmail.com>
2024-07-07 13:48:20 +0200

    Do the same for practice_locus

--- animation/practice_locus.py
+++ animation/practice_locus.py
@@ -7,15 +7,12 @@ class Practice_Locus(GenericSolveBlocks):
         self.default_color = RED
         self.transition()
 
-        self.introduce_first_problem()
         self.solve_first_problem()
         self.clear_blocks()
 
-        self.introduce_second_problem()
         self.solve_second_problem()
         self.clear_blocks()
 
-        self.introduce_third_problem()
         self.solve_third_problem()
     
     def transition(self):
@@ -24,45 +21,60 @@ class Practice_Locus(GenericSolveBlocks):
         self.add(transition_title)
         self.wait()
 
-        self.next_section("Make_space")
+        self.next_section("Show_Problems")
 
         title = Tex("Bestimme die Ortskurve aller Tiefpunkte", color=RED)
         title.scale(0.6)
-        title.move_to(UP * 3.5)
+
+        all_problems = [
+            r"f_a(x) = e^{x-a} - x",
+            r"f_a(x) = e^x - ax",
+            r"f_a(x) = e^x - a^2x; a \neq 0",
+        ]
+
+        all_problems_tex = VGroup(title)
+
+        for problem in all_problems:
+            problem_tex = MathTex(problem)
+            all_problems_tex.add(problem_tex)
+        
+        all_problems_tex.arrange(DOWN)
 
         self.play(
-            Transform(transition_title, title),
+            Transform(transition_title, title, replace_mobject_with_target_in_scene=True),
             run_time=0.5,
         )
-    
-    def introduce_first_problem(self):
-        equation = MathTex(r"f_a(x) = e^{x-a} - x")
-        self.blocks.append(equation)
-
-        page_num = Tex("Seite 165 Nummer 19")
-        page_num.scale(0.4)
-        page_num.move_to(LEFT * 5 + UP * 3.75)
-
         self.play(
-            Write(equation),
-            FadeIn(page_num, shift=DOWN),
-            run_time=1,
+            LaggedStart(
+                [Write(p) for p in all_problems_tex[1:]],
+                lag_ratio=0.3,
+                run_time=3,
+            ),
         )
         self.wait()
-
-        self.next_section("Make_room")
-
-        equation.generate_target()
-        equation.target.scale(0.8)
-        equation.target.move_to(UP * 3)
+        
+        self.next_section("Clear_Problems")
 
         self.play(
-            MoveToTarget(equation),
-            run_time=1
+            LaggedStart(
+                [Unwrite(p) for p in all_problems_tex],
+                lag_ratio=0.3,
+                run_time=3,
+            )
+        )
+
+        self.heading = Tex("Lösungen", color=RED)
+        self.heading.scale(0.6)
+        self.heading.move_to(UP * 3.5)
+
+        self.play(
+            FadeIn(self.heading, shift=DOWN),
+            run_time=0.7,
         )
     
     def solve_first_problem(self):
         solve_turning_point = [
+            r"f_a(x) = e^{x-a} - x",
             r"f'_a(x) = (1)e^{x-a} - 1",
             r"e^{x-a} - 1 = 0",
             r"e^{x-a} = 1",
@@ -82,29 +94,9 @@ class Practice_Locus(GenericSolveBlocks):
         self.block("Tiefpunkt", UP * 2, solve_height)
         self.block("Umwandeln", UP * 2 + RIGHT * 4, solve_finally)
     
-    def introduce_second_problem(self):
-        equation = MathTex(r"f_a(x) = e^x - ax")
-        self.blocks.append(equation)
-
-        self.play(
-            Write(equation),
-            run_time=1,
-        )
-        self.wait()
-
-        self.next_section("Make_room")
-
-        equation.generate_target()
-        equation.target.scale(0.8)
-        equation.target.move_to(UP * 3)
-
-        self.play(
-            MoveToTarget(equation),
-            run_time=1
-        )
-    
     def solve_second_problem(self):
         solve_turning_point = [
+            r"f_a(x) = e^x - ax",
             r"f'_a(x) = e^x - a",
             r"e^x - a = 0",
             r"e^x = a",
@@ -129,29 +121,9 @@ class Practice_Locus(GenericSolveBlocks):
         self.block("Nach a auflösen", UP * 2 + RIGHT * 5, solve_a_x)
         self.block("Einsetzen", RIGHT * 5, solve_function)
     
-    def introduce_third_problem(self):
-        equation = MathTex(r"f_a(x) = e^x - a^2x; a \neq 0")
-        self.blocks.append(equation)
-
-        self.play(
-            Write(equation),
-            run_time=1,
-        )
-        self.wait()
-
-        self.next_section("Make_room")
-
-        equation.generate_target()
-        equation.target.scale(0.8)
-        equation.target.move_to(UP * 3)
-
-        self.play(
-            MoveToTarget(equation),
-            run_time=1
-        )
-    
     def solve_third_problem(self):
         solve_turning_point = [
+            r"f_a(x) = e^x - a^2x; a \neq 0",
             r"f'_a(x) = e^x - a^2",
             r"e^x - a^2 = 0",
             r"e^x = a^2",
@@ -171,7 +143,7 @@ class Practice_Locus(GenericSolveBlocks):
             r"y = e^x - x e^x",
         ]
 
-        self.block("Extremstelle", UP * 2 + LEFT * 5, solve_turning_point)
-        self.block("Tiefpunkt", UP * 2, solve_height)
-        self.block("Nach a auflösen", UP * 2 + RIGHT * 5, solve_a_x)
-        self.block("Einsetzen", RIGHT * 5, solve_function)
+        self.block("Extremstelle", UP * 2 + LEFT * 4.25, solve_turning_point)
+        self.block("Tiefpunkt", UP * 2 + RIGHT * 1, solve_height)
+        self.block("Nach a auflösen", UP * 1.5 + RIGHT * 5, solve_a_x)
+        self.block("Einsetzen", RIGHT * 5 + DOWN * 1.5, solve_function)
